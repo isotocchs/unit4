aa8737dfaf0791bd977a78f48d04734f186c2a7a
IgnacioSoto <isoto@columbushs.com>
2021-10-19 11:19:03 -0400

    test

--- unit4/src/com/comlumbus/unit4/Unit4_2ForLoop.java
+++ unit4/src/com/comlumbus/unit4/Unit4_2ForLoop.java
@@ -1,5 +1,24 @@
 package com.comlumbus.unit4;
 
 public class Unit4_2ForLoop {
+	
+	public void forLoopTesting() {
+		
+	// for(initialization; Boolean expression;update;){
+	// Do something;
+	//}
+	//you know how many times its gonna run. Parameters defined at the start.
+		
+		for(int num =1; num<=5; num++) {
+			
+			System.out.println(num);
+			
+			
+		}
+		
+		System.out.println("End of for loop");
+		
+		
+	}
 
 }
